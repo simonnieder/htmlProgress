a82b05acbee2e622dffb8f06c4c41f40ac00a586
unknown <niederwolfsgruber16@gmail.com>
2020-10-23 23:24:48 +0200

    randomColorBetter

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var colors = [0, 30, 60, 90, 120, 150, 300, 330]
+
 function randomColor() {
-  document.body.style.filter = "hue-rotate(" + Math.floor(Math.random() * 360) + "deg)";
+  document.body.style.filter = "hue-rotate(" + colors[Math.floor(Math.random() * colors.length)] + "deg)";
+
 }
