41701db071f7bd5bd920825442d12a46a81f1122
unknown <niederwolfsgruber16@gmail.com>
2020-10-24 00:49:26 +0200

    colorChange2

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var colors = [0, 30, 60, 90, 120, 150, 300, 330]
+var colorNow = 0;
 
 function randomColor() {
-  document.body.style.filter = "hue-rotate(" + colors[Math.floor(Math.random() * colors.length)] + "deg)";
+  var color = Math.floor(Math.random() * colors.length);
+  document.body.style.filter = "hue-rotate(" + colors[color] + "deg)";
+  colorNow = color;
+}
 
+
+function cycleColors() {
+  if (colorNow == colors.length - 1)
+    colorNow = 0;
+  else
+    ++colorNow;
+  document.body.style.filter = "hue-rotate(" + colors[colorNow] + "deg)";
 }
