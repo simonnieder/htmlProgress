ffe58408bbca9de2cd3fca0e2c3e9602e1ed8a14
unknown <niederwolfsgruber16@gmail.com>
2020-10-24 02:29:47 +0200

    colorChangeAnimated

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 var colors = [0, 30, 60, 90, 120, 150, 300, 330]
-var colorNow = 0;
+var colorIndex = 0;
 
 function randomColor() {
   var color = Math.floor(Math.random() * colors.length);
   document.body.style.filter = "hue-rotate(" + colors[color] + "deg)";
-  colorNow = color;
+  colorIndex = color;
 }
 
 
 function cycleColors() {
-  if (colorNow == colors.length - 1)
-    colorNow = 0;
+  var oldColor = colors[colorIndex]
+  if (colorIndex == colors.length - 1)
+    colorIndex = 0;
   else
-    ++colorNow;
-  document.body.style.filter = "hue-rotate(" + colors[colorNow] + "deg)";
+    ++colorIndex;
+
+  var updatingColor = oldColor;
+  var goalColor = colors[colorIndex];
+  var intervallSpeed = 10;
+  if (oldColor == 330) {
+    goalColor = 360;
+  } else if (oldColor == 150) {
+    intervallSpeed = intervallSpeed / 5;
+  }
+  var id = setInterval(frame, intervallSpeed);
+
+  function frame() {
+    if (updatingColor == goalColor) {
+      clearInterval(id);
+    } else {
+      if (goalColor < oldColor) {
+        updatingColor--;
+      } else {
+        updatingColor++;
+      }
+      console.log(updatingColor)
+      document.body.style.filter = "hue-rotate(" + updatingColor + "deg)";
+    }
+  }
 }
